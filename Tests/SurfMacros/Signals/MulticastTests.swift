b01a97747ca8cdc9d7308d44a0adf51262ef1c82
Goldilocks97 <ivan1979ivan@icloud.com>
2024-05-24 14:49:29 +0300

    SPT-2157 macro tests + templates fixed

--- Tests/SurfMacros/Signals/MulticastTests.swift
+++ Tests/SurfMacros/Signals/MulticastTests.swift
@@ -6,7 +6,7 @@ import XCTest
 import SurfMacroBody
 import SurfMacrosSupport
 
-private let testMacros: [String: Macro.Type] = ["multicast": MulticastMacro.self]
+private let testMacros: [String: Macro.Type] = ["Multicast": MulticastMacro.self]
 #endif
 
 final class MulticastMacroTests: XCTestCase {
@@ -42,61 +42,311 @@ final class MulticastMacroTests: XCTestCase {
         }
     }
     
-    func testWhenThereIsGeneric() {
+    func testWhenThereIsAssociated() {
         assertMacroExpansion(
             """
             @Multicast
-            protocol BatSignal<Item> {
-
+            protocol BatSignal {
+                associatedtype Item
+                  
+                func methodA()
             }
             """,
             expandedSource: """
-            protocol BatSignal<Item> {
-
+            protocol BatSignal {
+                associatedtype Item
+                  
+                func methodA()
             }
             """,
-            diagnostics: [.init(message: "There should not be generic", line: 1, column: 1)],
+            diagnostics: [.init(message: "There should not be any associated types", line: 1, column: 1)],
+            macros: testMacros
+       )
+    }
+
+    func testWhenThereIsVariable() {
+        assertMacroExpansion(
+            """
+            @Multicast
+            protocol BatSignal {
+                var robinsName: String { get }
+                  
+                func methodA()
+            }
+            """,
+            expandedSource: """
+            protocol BatSignal {
+                var robinsName: String { get }
+                  
+                func methodA()
+            }
+            """,
+            diagnostics: [.init(message: "There should not be any variables", line: 1, column: 1)],
             macros: testMacros
        )
     }
     
-    
-    
-    func testWhenThereIsNoAppropriateFunc() {
+    func testWhenFuncIsStatic() {
         assertMacroExpansion(
             """
             @Multicast
             protocol BatSignal {
-                func foo()
+                static func methodA()
+
+                func methodB()
             }
-            let defaultSignals: [BatSignal] = []
             """,
             expandedSource: """
             protocol BatSignal {
-                func foo()
+                static func methodA()
+
+                func methodB()
             }
+            """,
+            diagnostics: [
+                .init(
+                    message: """
+                        The only allowed format of a function is the following:
+                            func <name>(<argument>, ...)
+                    """,
+                    line: 1,
+                    column: 1
+                )
+            ],
+            macros: testMacros
+       )
+    }
+    
+    func testWhenFuncThrows() {
+        assertMacroExpansion(
+            """
+            @Multicast
+            protocol BatSignal {
+                func methodA() throws
+
+                func methodB()
+            }
+            """,
+            expandedSource: """
+            protocol BatSignal {
+                func methodA() throws
+
+                func methodB()
+            }
+            """,
+            diagnostics: [
+                .init(
+                    message: """
+                        The only allowed format of a function is the following:
+                            func <name>(<argument>, ...)
+                    """,
+                    line: 1,
+                    column: 1
+                )
+            ],
+            macros: testMacros
+       )
+    }
+    
+    func testWhenFuncIsAsync() {
+        assertMacroExpansion(
+            """
+            @Multicast
+            protocol BatSignal {
+                func methodA() async
+
+                func methodB()
+            }
+            """,
+            expandedSource: """
+            protocol BatSignal {
+                func methodA() async
+
+                func methodB()
+            }
+            """,
+            diagnostics: [
+                .init(
+                    message: """
+                        The only allowed format of a function is the following:
+                            func <name>(<argument>, ...)
+                    """,
+                    line: 1,
+                    column: 1
+                )
+            ],
+            macros: testMacros
+       )
+    }
+    
+    func testWhenFuncHasAttribute() {
+        assertMacroExpansion(
+            """
+            @Multicast
+            protocol BatSignal {
+                @MainActor
+                func methodA()
+
+                func methodB()
+            }
+            """,
+            expandedSource: """
+            protocol BatSignal {
+                @MainActor
+                func methodA()
+
+                func methodB()
+            }
+            """,
+            diagnostics: [
+                .init(
+                    message: """
+                        The only allowed format of a function is the following:
+                            func <name>(<argument>, ...)
+                    """,
+                    line: 1,
+                    column: 1
+                )
+            ],
+            macros: testMacros
+       )
+    }
+    
+    func testWhenFuncReturnIsNotVoid() {
+        assertMacroExpansion(
+            """
+            @Multicast
+            protocol BatSignal {
+                func methodA() -> String
+
+                func methodB()
+            }
+            """,
+            expandedSource: """
+            protocol BatSignal {
+                func methodA() -> String
+
+                func methodB()
+            }
+            """,
+            diagnostics: [
+                .init(
+                    message: """
+                        The only allowed format of a function is the following:
+                            func <name>(<argument>, ...)
+                    """,
+                    line: 1,
+                    column: 1
+                )
+            ],
+            macros: testMacros
+       )
+    }
+    
+    func testWhenFuncIsGenericWithoutWhereKeyword() {
+        assertMacroExpansion(
+            """
+            @Multicast
+            protocol BatSignal {
+                func methodA<T>(input: T)
+
+                func methodB()
+            }
+            """,
+            expandedSource: """
+            protocol BatSignal {
+                func methodA<T>(input: T)
+
+                func methodB()
+            }
+            """,
+            diagnostics: [
+                .init(
+                    message: """
+                        The only allowed format of a function is the following:
+                            func <name>(<argument>, ...)
+                    """,
+                    line: 1,
+                    column: 1
+                )
+            ],
+            macros: testMacros
+       )
+    }
+    
+    func testWhenFuncIsGenericWithWhereKeyword() {
+        assertMacroExpansion(
+            """
+            @Multicast
+            protocol BatSignal {
+                func methodA<T>(input: T) where T: Codable
+
+                func methodB()
+            }
+            """,
+            expandedSource: """
+            protocol BatSignal {
+                func methodA<T>(input: T) where T: Codable
+
+                func methodB()
+            }
+            """,
+            diagnostics: [
+                .init(
+                    message: """
+                        The only allowed format of a function is the following:
+                            func <name>(<argument>, ...)
+                    """,
+                    line: 1,
+                    column: 1
+                )
+            ],
+            macros: testMacros
+       )
+    }
+
+
+    func testWithAllPossibleArgumentFormats() {
+        assertMacroExpansion(
+            """
+            @Multicast
+            protocol BatSignal {
+                func call(robin: Robin)
+                func call(for robin: Robin)
+                func call(_ robin: Robin)
+            }
+            """,
+            expandedSource: """
+            protocol BatSignal {
+                func call(robin: Robin)
+                func call(for robin: Robin)
+                func call(_ robin: Robin)
+            }
+
             public final class BatSignals: BatSignal {
                 private let signals: [BatSignal]
                 public init(@ArrayBuilder<BatSignal> _ signals: () -> [BatSignal]) {
                     self.signals = defaultSignals + signals()
                 }
-                func foo() {
+                public func call(robin: Robin) {
                     signals.forEach {
-                        $0.foo()
+                        $0.call(robin: robin)
+                    }
+                }
+                public func call(for robin: Robin) {
+                    signals.forEach {
+                        $0.call(for: robin)
+                    }
+                }
+                public func call(_ robin: Robin) {
+                    signals.forEach {
+                        $0.call(robin)
                     }
                 }
             }
-            let defaultSignals: [BatSignal] = []
             """,
             macros: testMacros
        )
     }
     
 }
-
-// Tests:
-// 1. Not a protocol
-// 2. There is an assocciated type
-// 3. There is a variable
-// 3. All functions that are unappropriate
-// 4. All types of parameter styles in functions (3)
