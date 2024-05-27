dae30744acb5ee219a50ee2b00c2bacc4918cd44
Goldilocks97 <ivan1979ivan@icloud.com>
2024-05-27 13:23:29 +0300

    SPT-2157 array-builder dock + impoves + macro doc imporves

--- Sources/SurfCore/ArrayBuilder/ArrayBuilder.swift
+++ Sources/SurfCore/ArrayBuilder/ArrayBuilder.swift
@@ -7,6 +7,26 @@
 
 import Foundation
 
+/// Всеядный билдер для декларативных конструкций.
+///
+/// Например,
+///```swift
+///SomeItems {
+///    Item1
+///    Item2
+///    if condition {
+///        Item3a
+///    } else {
+///        Item3b
+///    }
+///}
+///```
+/// где `SomeItems` - это короткая функция
+/// ```swift
+///public func SomeItems(@ArrayBuilder<ItemType>_ content: () -> [ItemType]) -> [ItemType] {
+///    content()
+///}
+///```
 @resultBuilder
 public enum ArrayBuilder<Item> {
 
@@ -36,6 +56,10 @@ public enum ArrayBuilder<Item> {
 
     public static func buildEither(second component: [Item]) -> [Item] {
         return component
+    }
+
+    public static func buildOptional(_ component: [Item]?) -> [Item] {
+        return component ?? []
     }
 
 }
--- Sources/SurfMacros/Macros/Signals/Multicast.swift
+++ Sources/SurfMacros/Macros/Signals/Multicast.swift
@@ -1,5 +1,6 @@
 /// Declares a class for general signal object that contains both inner module signals and ones set by user
 /// - Warning: Can be attached to a protocol only.
 /// - Warning: There must be `defaultSignals: [<SignalProtocol>]` property that contains inner module signals nearby the protocol declaration the macro attached to.
+/// - Warning: `SurfCore` module import is required. Don't forget to add it as a dependency in the manifest as well.
 @attached(peer, names: suffixed(s))
 public macro Multicast() = #externalMacro(module: "SurfMacroBody", type: "MulticastMacro")
